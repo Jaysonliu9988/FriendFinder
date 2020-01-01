442932b75fc66e08cf590bda61cffb4cd1311658
Jaysonliu <leobest9688@gmail.com>
2020-01-01 19:22:38 +0800

    friends.js done

---
+++ app/data/friends.js
@@ -0,0 +1,103 @@
+var friends = [
+    {
+      name: "Angel",
+      photo: "https://pbs.twimg.com/profile_images/690844636353433600/MisYE_FI_400x400.jpg",
+      scores: [
+        "5",
+        "2",
+        "4",
+        "4",
+        "5",
+        "1",
+        "3",
+        "5",
+        "5",
+        "1"
+      ]
+    },
+    {
+      name: "Cindy",
+      photo: "https://pbs.twimg.com/profile_images/2921817323/6b8edd3a78795c8d85dd586c0639e078.jpeg",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "3",
+        "3",
+        "2",
+        "1",
+        "1",
+        "3",
+        "5"
+      ]
+    },
+    {
+      name: "Lily",
+      photo: "https://pbs.twimg.com/profile_images/2702786635/1176f41703478c3c80eb92bae85a5898.png",
+      scores: [
+        "4",
+        "2",
+        "3",
+        "3",
+        "4",
+        "1",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    },
+    {
+      name: "Lucy",
+      photo: "https://pbs.twimg.com/profile_images/717889359521841152/sDTmuT46_400x400.jpg",
+      scores: [
+        "5",
+        "3",
+        "4",
+        "2",
+        "3",
+        "1",
+        "3",
+        "2",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Milk",
+      photo: "https://pbs.twimg.com/profile_images/712354813304942594/tNBQ12Yx_400x400.jpg",
+      scores: [
+        "2",
+        "3",
+        "5",
+        "1",
+        "5",
+        "2",
+        "5",
+        "4",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Sharon",
+      photo: "https://pbs.twimg.com/profile_images/703996925364695040/xnaqUKZ8_400x400.jpg",
+      scores: [
+        "5",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "4",
+        "1",
+        "4",
+        "5"
+      ]
+    }
+  
+  ];
+  
+  
+  module.exports = friends;
+  
